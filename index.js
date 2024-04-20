5920dbd56d369410c204d14c840a576f5a7baa84
vermasiddhartha04 <146537179+vermasiddhartha04@users.noreply.github.com>
2024-04-20 04:43:29 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+prompt ("what is your name");
+prompt ("what is their name");
+var lovescore = Math.random() * 100;
+lovescore = Math.floor(lovescore)+1;
+alert("this is your love score" + lovescore +"%");
